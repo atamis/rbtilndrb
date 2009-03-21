5d282915eb2b308a0c0a9f1725a965117ad2cd93
indigo747 <atamiser@gmail.com>
2009-03-21 13:41:48 -0700

    Defed jump. Defed Proccess input

--- lib/general.rb
+++ lib/general.rb
@@ -19,10 +19,62 @@
 #       MA 02110-1301, USA.
 
 
-class StdClass
-	def initialize
-		
+def jump
+	$year += 1
+	ml = []
+	fl = []
+	$mrabbits.each { |r| r.next }
+	$frabbits.each { |r| r.next }
+	$mrabbits.each { |m| ml[ml.length] = m}
+	$frabbits.each { |f| fl[fl.length] = f}
+	if ml.length <= fl.length
+		ml.each {|m| m.breed}
+		ml.length.times do
+			counter = ml.length
+			until counter == 0
+				if fl[counter].available?
+					fl[counter].breed
+				end
+				counter -= 1
+			end
+		end 
 	end
+	$mrabbits.each { |r| r.next}
+	$frabbits.each { |r| r.next}
 end
 
-x = StdClass.new
+def info
+	results = "Year: #{$year}
+Male Rabbits: #{$mrabbits.length}
+Female Rabbits: #{$frabbits.length}"
+	return results
+end
+
+def proccess_input(input, arg)
+	case input
+		when "e"
+			puts "Exiting! Come back soon!"
+			exit
+		when "r"
+			puts "Reloading program. This starts a new program and kills this one."
+			puts
+			puts
+			system("ruby start_rbtilndrb.rb")
+			exit
+		when "d"
+			puts "This is the debug center. You may or may not see strange numbers here."
+			puts info
+			case arg
+				when "test"
+					print "What variable do you want? (make sure it exists. Program may crash if it doesn't): "
+					var = gets.chomp
+					puts "Printing #{var}"
+					puts eval("puts #{var}")
+			end
+		when "i"
+			puts "Status:"
+			puts info
+		when "j"
+			jump
+		end
+end
