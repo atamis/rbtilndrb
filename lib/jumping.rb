05a82dc762237998fff6c01320ea267f7ff4a4b9
indigo747 <atamiser@gmail.com>
2009-03-24 11:09:15 -0700

    Implimented new breeding system. Doesn't work yet.

--- lib/jumping.rb
+++ lib/jumping.rb
@@ -25,26 +25,39 @@ def jump
 	fl = []
 	$mrabbits.each { |r| r.next }
 	$frabbits.each { |r| r.next }
-	$mrabbits.each { |m| ml.push(m) if m.available?}
-	$frabbits.each { |f| fl.push(f) if f.available?}
-	if ml.length <= fl.length
-		counter = 0
-		ml.length.times do 
-			make_new(ml[counter], fl[counter])
-			counter += 1
-		end
-	else
-		counter = 0
-		fl.length.times do
-			make_new(ml[counter], fl[counter])
-			counter += 1
+	mr_there = false
+	$mrabbits.each do |mr|
+		if mr.available?
+			mr_there = true
 		end
 	end
+	fl_there = false
+	$frabbits.each do |fr|
+		if fr.available?
+			fl_there = true
+		end
+	end
+	if mr_there && fl_there
+		$frabbits.length.times {make_new}
+	end
+#	$mrabbits.each { |m| ml.push(m) if m.available?}
+#	$frabbits.each { |f| fl.push(f) if f.available?}
+#	if ml.length <= fl.length
+#		counter = 0
+#		ml.length.times do 
+#			make_new(ml[counter], fl[counter])
+#			counter += 1
+#		end
+#	else
+#		counter = 0
+#		fl.length.times do
+#			make_new(ml[counter], fl[counter])
+#			counter += 1
+#		end
+#	end
 end
 
 def make_new(m, f)
-	m.breed
-	f.breed
 	(rand(7)+3).times do
 		if rand(2) == 0
 			$mrabbits.push(Rabbit.new("m"))
