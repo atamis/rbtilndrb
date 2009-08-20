14e2d627861a5f7c053f57f37623400eaa8a81ed
Indigo Casson <indigo@indigo-acer-laptop.cruzio.com>
2009-08-20 09:34:16 -0700

    Added male tests and started female tests

--- lib/tc_rabbit.rb
+++ lib/tc_rabbit.rb
@@ -1,11 +1,26 @@
 require "test/unit"
 
-require "rabbit"
+require "rabbit.rb"
 
 class TestRabbit < Test::Unit::TestCase
-  m = Rabbit.new(m)
-  f = Rabbit.new(f)
-  def test_case_name
-    assert(m.alive?, "M not alive.")
+  @@m = Rabbit.new("m")
+  @@f = Rabbit.new("f")
+  def default_test
+    assert_alive
+    male_tests
   end
+  def assert_alive
+    assert_equal(true, @@m.alive?)
+    assert_equal(true, @@f.alive?)
+  end
+  
+  def male_tests
+    rabbit = @@m
+    assert_equal("male", rabbit.gender)
+    assert_equal(true, rabbit.male?)
+    assert_equal(false, rabbit.female?)
+  end
+  
 end
+
+
